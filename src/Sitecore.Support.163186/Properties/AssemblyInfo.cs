d416181e66d3d93062a035cae67b120e415ad898
DK\msb <msb@sitecore.net>
2017-05-24 14:55:06 -0400

    Revert "Search query was changed. Visits are loaded in batches."
    This reverts commit b21e073f4adaa22ed5e1d8e72a5d9791ec365d27.

--- src/Sitecore.Support.163186/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Reflection;
-using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("Sitecore.Support.163186")]
-[assembly: AssemblyProduct("Sitecore.Support.163186")]
-[assembly: ComVisible(false)]
