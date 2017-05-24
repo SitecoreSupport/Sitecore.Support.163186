1706cc9d9bec7e7f277816d4f30a50a9cbf90ff5
DK\msb <msb@sitecore.net>
2017-05-24 16:43:35 -0400

    Reupload all files

---
+++ src/Sitecore.Support.163186/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.155407")]
+[assembly: AssemblyProduct("Sitecore.Support.155407")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("It's a clean assembly. Modification has been added to the sitecore/shell/Controls/JqueryModalDialogs.html file")]
+
